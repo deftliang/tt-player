ef61efe3cac813b7223cf6aa2b41c4fae9c04288
deft <deftmail@163.com>
2022-03-08 22:17:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #2c3e50;
+  height: 100vh;
+}
+</style>
